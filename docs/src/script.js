ff935faf3b141d2f41d9b8927d59060e59e14efd
Satyam Verma <satyamverma46@outlook.com>
2024-03-06 21:24:44 +0530

    nah a SMAL docs update

---
+++ docs/src/script.js
@@ -0,0 +1,16 @@
+url = "md/DOCS.md";
+$.ajax({
+    url: url,
+    type: "GET",
+    dataType: "text",
+    success: function (results) {
+        var converter = new showdown.Converter();
+        converter.setFlavor('github');
+        var html = converter.makeHtml(results);
+        document.querySelector('#root').innerHTML = html
+    }
+});
+document.getElementById('toggle').addEventListener('click', () => {
+    document.getElementById('blender').classList.toggle('expand');
+    document.getElementById('toggle').classList.toggle('module--zIndex');
+});
