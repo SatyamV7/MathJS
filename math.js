2d888753dbba79caa05233654ec98bdf21c01866
Satyam Verma <satyamverma46@outlook.com>
2024-04-23 22:58:06 +0530

    Update version to 1.1.6 and update SemVer.js script

--- math.js
+++ math.js
@@ -1,6 +1,6 @@
 "use strict";
 /*
-    MathJS v1.1.5
+    MathJS v1.1.6
     Last Modified: 23/04/2024 <DD/MM/YYYY>
     Author: Satyam Verma <github.com/SatyamV7>
     Description: A JavaScript library for basic and advanced arithmetic operations, Satistical functions, logical functions, factorial and fibonacci functions, random number functions, and trigonometric functions.
